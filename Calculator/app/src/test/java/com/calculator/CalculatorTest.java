43418f3208771ae0d31de56a4a2e9affb14e2502
Jasvir <kaurjasvir214@gmail.com>
2018-11-19 23:30:27 -0500

    Renamed Test class name

--- Calculator/app/src/test/java/com/calculator/CalculatorTest.java
+++ Calculator/app/src/test/java/com/calculator/CalculatorTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertThat;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class CalculatorTest {
 
 
     @Test
